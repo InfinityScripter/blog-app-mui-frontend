df9ccc302da9f5723b543f1279be987479f0aa21
InfinityScripter <talalaev.misha@gmail.com>
2025-02-27 12:25:55 +0300

    update metadata

--- src/app/(index)/page.jsx
+++ src/app/(index)/page.jsx
@@ -3,9 +3,9 @@ import { HomeView } from 'src/sections/home/view';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Minimals UI: The starting point for your next project',
+  title: 'Mihail Talalaev | Web blog',
   description:
-    'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
+    'Web blog for frontend developer. I write about web development, design, and other topics.',
 };
 
 export default function Page() {
